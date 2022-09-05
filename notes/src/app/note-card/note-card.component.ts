ceb4ddb3f10f27a0b20b5793e07cdf114ea9ad1d
My Name <hariprakash@sparkouttech.com>
2022-09-05 14:25:30 +0530

    New notes added

--- notes/src/app/note-card/note-card.component.ts
+++ notes/src/app/note-card/note-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
 export class NoteCardComponent implements OnInit {
-
-  constructor() { }
+  
+  @Input()title:string;
+  @Input()body:string;
+  constructor() {}
 
   ngOnInit(): void {
-  }
+   
 
+
+  }
 }
