a017fc0daec0e022fdd418e1d75da062a88a4ab8
My Name <hariprakash@sparkouttech.com>
2022-09-05 18:15:14 +0530

    search filter

--- notes/src/app/pages/note-list/note-list.component.ts
+++ notes/src/app/pages/note-list/note-list.component.ts
@@ -1,21 +1,59 @@
+import { r3JitTypeSourceSpan } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { NotesService } from 'src/app/shared/notes.service';
 import { Note } from '../../shared/note.module';
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
-  styleUrls: ['./note-list.component.scss']
+  styleUrls: ['./note-list.component.scss'],
 })
 export class NoteListComponent implements OnInit {
- 
-  notes:Note[]= new Array<Note>();
-  constructor(private noteService:NotesService) { }
+  notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
+  constructor(private noteService: NotesService) {}
 
   ngOnInit(): void {
-    this.notes=this.noteService.getAll()
+    this.notes = this.noteService.getAll();
+    this.filteredNotes = this.notes;
   }
 
-  deleteNote(id:number){
-    this.noteService.delete(id)
+  deleteNote(id: number) {
+    this.noteService.delete(id);
+  }
+  filter(query: string) {
+    console.log(query);
+    
+    query = query.toLowerCase().trim();
+    let allResults: Note[] = new Array<Note>();
+    let terms: string[] = query.split('');
+
+    terms = this.removeDuplicate(terms);
+
+    terms.forEach(term => {
+      let results: Note[] = this.releventNotes(term);
+      allResults = [...allResults, ...results];
+    });
+
+    let uniqueResult = this.removeDuplicate(allResults);
+    this.filteredNotes = uniqueResult;
+  }
+  removeDuplicate(arr: Array<any>): Array<any> {
+    let uniqueResult: Set<any> = new Set<any>();
+    arr.forEach((e) => uniqueResult.add(e));
+    return Array.from(uniqueResult);
+  }
+  releventNotes(query: any): Array<Note> {
+    query = query.toLowerCase().trim();
+    let releventNotes = this.notes.filter(note => {
+      if (note.title && note.title.toLowerCase().includes(query)) {
+        return true;
+      }
+      if (note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }else{
+      return this.filteredNotes = this.notes;}
+    });
+    return releventNotes;
+    
   }
 }
