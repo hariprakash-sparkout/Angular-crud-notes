8378cf5113e1ff7376a5bcfecb61c09da968b852
My Name <hariprakash@sparkouttech.com>
2022-09-05 11:41:18 +0530

    initial

---
+++ notes/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoteListComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
