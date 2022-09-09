efc24646c74ae8cb604ec5976f3123455d357831
My Name <hariprakash@sparkouttech.com>
2022-09-09 12:55:01 +0530

    fixes

--- crud/src/app/pages/edit-user/edit-user.component.ts
+++ crud/src/app/pages/edit-user/edit-user.component.ts
@@ -39,23 +39,15 @@ export class EditUserComponent implements OnInit {
     this.formValue = this.formbuilder.group({
       name: [
         '',
-        [Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(30)]
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
       ],
       email: ['', [Validators.required, Validators.email]],
-      number: [
-        '',
-        [Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(10)]
-      ],
-      salary: [
-        '',
-        [Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(30)]
-      ],
+      number: ['', [Validators.required, Validators.pattern('[0-9]{10,}')]],
+      salary: ['', [Validators.required, Validators.pattern('[0-9]{3,}')]],
     });
     console.log(this.formValue.valid);
     this.api.getEditUserDetails().subscribe((res: any) => {
